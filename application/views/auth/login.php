cdb0728071a7f1dec2e7e90642089995b9a6fc5b
Alda Amorita Azza <aldaamorita21@if.unjani.ac.id>
2024-08-13 10:15:31 +0700

    update

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,14 +1,9 @@
-
-
-    <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-lg-7">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
+<div class="container">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+        <div class="col-lg-7">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
                             <div class="col-lg">
