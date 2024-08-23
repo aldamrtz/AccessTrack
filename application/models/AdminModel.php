d684827f8b3f7183c79576cfa5947f72d585a6ef
aminuddinihsan20 <aminuddin.ihsan47@gmail.com>
2024-08-23 14:55:23 +0700

    menambahkan tampilan login

---
+++ application/models/AdminModel.php
@@ -0,0 +1,16 @@
+<?php
+class AdminModel {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function getAdminByUsername($username) {
+        $stmt = $this->db->prepare("SELECT * FROM admins WHERE username = :username");
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
