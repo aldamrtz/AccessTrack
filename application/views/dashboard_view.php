e6c7c007b4f0d7551c8c4cf8b87a95ba9773d6b2
VictusWell <ferdiansyahbase9@gmail.com>
2024-11-08 07:47:08 +0700

    menghilangkan Button Fakultas dan jurusan

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -255,33 +255,6 @@
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3 d-flex justify-content-between align-items-center">
                                     <h6 class="m-0 font-weight-bold text-success">Total Laporan</h6>
-                                    <!-- Dropdown Button -->
-                                    <!-- Dropdown Container -->
-                                    <div class="d-flex align-items-center">
-                                        <!-- First Dropdown Button -->
-                                        <div class="d-flex align-items-center">
-                                            <!-- Fakultas Dropdown -->
-                                            <div class="dropdown mr-2">
-                                                <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    Fakultas
-                                                </button>
-                                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton1" id="fakultasDropdown">
-                                                    <!-- Fakultas options will be dynamically added here -->
-                                                </div>
-                                            </div>
-
-                                            <!-- Jurusan Dropdown -->
-                                            <div class="dropdown">
-                                                <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    Jurusan
-                                                </button>
-                                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton2" id="jurusanDropdown">
-                                                    <!-- Jurusan options will be dynamically added here -->
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
                                 </div>
                                 <div class="card-body">
                                     <canvas id="my3DBarChart"></canvas>
@@ -432,119 +405,6 @@
                     }
                 });
 
-                // Data for Fakultas and Jurusan
-                const fakultasJurusan = {
-                    'Teknik (FT)': [
-                        'Teknik Elektro S-1',
-                        'Teknik Kimia S-1',
-                        'Teknik Sipil S-1',
-                        'Magister Teknik Sipil S-2',
-                        'Teknik Geomatika S-1'
-                    ],
-                    'Teknologi Manufaktur (FTM)': [
-                        'Teknik Mesin S-1',
-                        'Teknik Industri S-1',
-                        'Teknik Metalurgi S-1',
-                        'Magister Manajemen Teknologi S-2'
-                    ],
-                    'Ekonomi dan Bisnis (FEB)': [
-                        'Akuntansi S-1',
-                        'Manajemen S-1',
-                        'Magister Manajemen S-2'
-                    ],
-                    'Ilmu Sosial dan Ilmu Politik (FISIP)': [
-                        'Ilmu Pemerintahan S-1',
-                        'Ilmu Hub. Internasional S-1',
-                        'Magister Hub. Internasional S-2',
-                        'Ilmu Hukum S-1',
-                        'Magister Ilmu Pemerintahan S-2'
-                    ],
-                    'Sains dan Informatika (FSI)': [
-                        'Kimia S-1',
-                        'Magister Kimia S-2',
-                        'Teknik Informatika S-1',
-                        'Sistem Informasi S-1'
-                    ],
-                    'Psikologi': [
-                        'Psikologi S-1'
-                    ],
-                    'Farmasi': [
-                        'Farmasi S-1',
-                        'Profesi Apoteker',
-                        'Magister Farmasi S-2'
-                    ],
-                    'Kedokteran': [
-                        'Pendidikan Dokter S-1',
-                        'Profesi Dokter',
-                        'Administrasi Rumah Sakit S-1',
-                        'Magister Penuaan Kulit dan Estetika S-2'
-                    ],
-                    'Kedokteran Gigi': [
-                        'Kedokteran Gigi S-1',
-                        'Profesi Dokter Gigi'
-                    ],
-                    'FITKES': [
-                        'Magister Keperawatan S-2',
-                        'Profesi Ners',
-                        'Ilmu Keperawatan S-1',
-                        'Keperawatan D-3',
-                        'Kesehatan Masyarakat S-1',
-                        'Teknologi Laboraturium Medis D-4',
-                        'Teknologi Laboraturium Medis D-3',
-                        'Kebidanan S-1',
-                        'Profesi Bidan',
-                        'Kebidanan D-3',
-                        'Magister Kesehatan Masyarakat S-2'
-                    ]
-                };
-
-                const fakultasDropdown = document.getElementById('fakultasDropdown');
-                const dropdownFakultasButton = document.getElementById('dropdownMenuButton1');
-                const jurusanDropdown = document.getElementById('jurusanDropdown');
-                const dropdownJurusanButton = document.getElementById('dropdownMenuButton2');
-
-                // Populate Fakultas Dropdown
-                Object.keys(fakultasJurusan).forEach(fakultasName => {
-                    const item = document.createElement('a');
-                    item.className = 'dropdown-item';
-                    item.href = '#';
-                    item.dataset.fakultas = fakultasName;
-                    item.textContent = fakultasName;
-                    fakultasDropdown.appendChild(item);
-                });
-
-                // Handle Fakultas Dropdown Selection
-                fakultasDropdown.addEventListener('click', function(event) {
-                    const target = event.target;
-                    if (target.classList.contains('dropdown-item')) {
-                        const fakultasName = target.dataset.fakultas;
-                        dropdownFakultasButton.textContent = fakultasName; // Update Fakultas button text
-                        jurusanDropdown.innerHTML = ''; // Clear existing jurusan
-                        dropdownJurusanButton.textContent = 'Jurusan'; // Reset Jurusan button text
-
-                        // Populate Jurusan Dropdown based on selected Fakultas
-                        if (fakultasJurusan[fakultasName]) {
-                            fakultasJurusan[fakultasName].forEach(jurusan => {
-                                const item = document.createElement('a');
-                                item.className = 'dropdown-item';
-                                item.href = '#';
-                                item.textContent = jurusan;
-                                item.dataset.jurusan = jurusan;
-                                jurusanDropdown.appendChild(item);
-                            });
-                        }
-                    }
-                });
-
-                // Handle Jurusan Dropdown Selection
-                jurusanDropdown.addEventListener('click', function(event) {
-                    const target = event.target;
-                    if (target.classList.contains('dropdown-item')) {
-                        const jurusanName = target.dataset.jurusan;
-                        dropdownJurusanButton.textContent = jurusanName; // Update Jurusan button text
-                    }
-                });
-
                 // Event listener untuk Kartu Akses
                 document.getElementById('detailKartuAkses').addEventListener('click', function(e) {
                     e.preventDefault();
@@ -642,7 +502,7 @@
                     my3DBarChart.data = {
                         labels: ['Domain Diajukan', 'Domain Diproses', 'Domain Terverifikasi', 'Domain Dikirimkan'],
                         datasets: [{
-                            label: 'Pengajuan Email',
+                            label: 'Pengajuan Domain',
                             data: [
                                 <?= $dashboard_data['pengajuan_domain_diajukan']; ?>,
                                 <?= $dashboard_data['pengajuan_domain_diproses']; ?>,
